d35253abe9d513d122c051611a9186f20f8ed066
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-03-05 14:52:01 +0300

    Create Login View model

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceAgenciesDatabaseApp.ViewModel
+{
+    public class LoginViewModel
+    {
+    }
+}
