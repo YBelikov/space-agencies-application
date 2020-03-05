bca38cc9aa8865f9e969327c8eafcf4071a93ff9
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-03-05 20:54:58 +0300

    Create Index view for Roles controller

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SpaceAgenciesDatabaseApp.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SpaceAgenciesDatabaseApp.Models;
 
 namespace SpaceAgenciesDatabaseApp.Controllers
 {
     public class RolesController : Controller
     {
-        public IActionResult Index()
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
         {
-            return View();
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index() 
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+        public IActionResult UserList()
+        {
+            return View(_userManager.Users.ToList());
+        }
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    AllRoles = allRoles,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles
+                };
+                return View(model);
+            }
+                return NotFound();  
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if(user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+                await _userManager.AddToRolesAsync(user, addedRoles);
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+                return RedirectToAction("UserList");
+            }
+            return NotFound();
         }
     }
-}
+
+}   
